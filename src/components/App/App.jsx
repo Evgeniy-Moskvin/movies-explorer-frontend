53ffa06aa83db70e84bedbe191d70260710a12ce
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-09-13 01:48:51 +0300

    feat: сверстать компонент Techs

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import './App.css';
 
+import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
 import Portfolio from '../Portfolio/Portfolio';
 import Footer from '../Footer/Footer';
@@ -8,6 +9,7 @@ import Footer from '../Footer/Footer';
 function App() {
   return (
     <>
+      <Techs/>
       <AboutMe/>
       <Portfolio/>
       <Footer/>
