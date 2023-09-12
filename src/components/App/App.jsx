cf2af49fead2375d79a809520d4cd8cff1fd2821
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-09-13 00:59:45 +0300

    feat: сверстать компонент Portfolio

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 
+import Portfolio from '../Portfolio/Portfolio';
 import Footer from '../Footer/Footer';
 
 
 function App() {
   return (
-    <Footer/>
+    <>
+      <Portfolio/>
+      <Footer/>
+    </>
   )
 }
 
