edf1c40eb9101732d8d5a5b920e8a3de18c3e3f0
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-09-13 03:05:16 +0300

    feat: создать компонент Main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,25 +1,24 @@
 import './App.css';
 
-import Promo from '../Promo/Promo';
-import NavTab from '../NavTab/NavTab';
-import AboutProject from '../AboutProject/AboutProject';
-import Techs from '../Techs/Techs';
-import AboutMe from '../AboutMe/AboutMe';
-import Portfolio from '../Portfolio/Portfolio';
+import { Routes, Route } from 'react-router-dom';
+
+import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 
 
 function App() {
   return (
-    <>
-      <Promo/>
-      <NavTab/>
-      <AboutProject/>
-      <Techs/>
-      <AboutMe/>
-      <Portfolio/>
-      <Footer/>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Main/>
+            <Footer/>
+          </>
+        }
+      />
+    </Routes>
   )
 }
 
