dccf733e376916d8eba2bd3e943ed5d30c816130
Evgeniy-Moskvin <moskwin.evgeny@gmail.com>
2023-09-19 16:57:55 +0300

    fix: not-found__link profile__controls - расположить элементы в поточном режиме

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -5,8 +5,10 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="not-found">
-      <p className="not-found__error-code">404</p>
-      <h1 className="not-found__title">Страница не найдена</h1>
+      <div className="not-found__content">
+          <p className="not-found__error-code">404</p>
+          <h1 className="not-found__title">Страница не найдена</h1>
+      </div>
 
       <Link to="/" className="not-found__link link-animate">Назад</Link>
     </div>
